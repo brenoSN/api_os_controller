7ffb52d68fc2e1e2c7cc079454ea342c245ed6ec
brenoSN <99511303+brenoSN@users.noreply.github.com>
2022-07-29 18:43:52 -0300

    add post config

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -6,7 +6,7 @@ const initDatabase = (req, res) => {
 };
 
 const getStatus = (req, res) => {
-    const sqlQuery = 'SELECT status FROM task_status';
+    const sqlQuery = 'SELECT * FROM task_status';
 
     database.query(sqlQuery, (err, result) => {
         if (err) throw err;
@@ -15,8 +15,31 @@ const getStatus = (req, res) => {
     });
 };
 
-const addSubscriber = (req, res) => {
 
+const addSubscriber = (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.array().length > 0) {
+        res.send(errors.array());
+    } else {
+        const task = {
+            id: req.body.id,
+            name: req.body.name,
+            description: req.body.description,
+            creationDate: req.body.creationDate,
+            lastEditDate: req.body.lastEditDate,
+            time: req.body.time,
+            status: req.body.status,
+        };
+
+        const sqlQuery = 'INSERT INTO task SET ?';
+
+        database.query(sqlQuery, task, (err, row) => {
+            if (err) throw err;
+
+            res.send('Subscribed successfully!');
+        });
+    }
 };
 
 module.exports = {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,13 @@ const router = new express.Router();
 router.get('/init', controller.initDatabase);
 router.get('/status', controller.getStatus);
 router.post('/subscribe', 
+    body('id').not().isEmpty().escape(),
     body('name').not().isEmpty().escape(),
+    body('description'),
+    body('creationDate').not().isEmpty().escape(),
+    body('lastEditDate').not().isEmpty().escape(),
+    body('time').not().isEmpty().escape(),
+    body('status').not().isEmpty().escape(),
     controller.addSubscriber    
 );
 
